8062cb22fb0d904cd7b6a076e12c68d1842833c4
mariaro833 <masha.rozhnova@gmail.com>
2022-05-08 22:51:15 +0300

    working version

--- js_cylinder/js/script.js
+++ js_cylinder/js/script.js
@@ -9,7 +9,7 @@ window.onload = function(){
 	var renderer = new THREE.WebGLRenderer({canvas: canvas});
 	renderer.setClearColor('black');
 
-	var scene = new THREE.Scene();
+	const scene = new THREE.Scene();
 
 	var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
 	camera.position.set(0, 0, 1000);
@@ -17,10 +17,44 @@ window.onload = function(){
 	var light = new THREE.AmbientLight('white');
 	scene.add(light);
 
-	var geometry = new THREE.CylinderGeometry(100, 100, 150, 100, 100);
-	var material = new THREE.MeshBasicMaterial({color: 'blue', wireframe: true});
-	var mesh = new THREE.Mesh(geometry, material);
+
+	const geometry = new THREE.CylinderGeometry(200, 200, 250, 33, 30, 8, 8, 8);
+	const material = new THREE.MeshBasicMaterial({color: 'blue', wireframe: true});
+
+	// for(var i = 0; i < geometry.faces.length; i++){
+	// 	geometry.faces[i].color.setRGB(Math.random(), Math.random(), Math.random());
+	// }
+
+	const mesh = new THREE.Mesh(geometry, material);
 	scene.add(mesh);
 
-	renderer.render(scene, camera);
+	var gui = new dat.GUI();
+
+	var ball = {
+		rotationY: 0,
+		scaleZ: 0,
+		scaleY: 0,
+		scaleX: 0
+	};
+
+	gui.add(ball, 'rotationY').min(-0.2).max(0.2).step(0.0001);
+	gui.add(ball, 'scaleZ').min(100).max(500).step(0.0001);
+	gui.add(ball, 'scaleY').min(80).max(500).step(0.0001);
+	gui.add(ball, 'scaleX').min(80).max(500).step(0.0001);
+	function loop(){
+		mesh.rotation.y += ball.rotationY;
+		mesh.scale.z += ball.scaleZ;
+		mesh.scale.y += ball.scaleY;
+		mesh.scale.x += ball.scaleX;
+		mesh.scale.y <= 500;
+		mesh.scale.x <= 500;
+		mesh.scale.z <= 500;
+		mesh.scale.y = mesh.scale.x;
+
+
+		renderer.render(scene, camera);
+		requestAnimationFrame(function(){loop();});
+	}
+
+	loop();
 }
